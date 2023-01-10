7585ab66be9b25402cba4343a72132b5f318664d
ridsaertavans <82870824+ridsaertavans@users.noreply.github.com>
2023-01-10 20:51:20 +0100

    Update dht11.py

--- dht11.py
+++ dht11.py
@@ -2,17 +2,17 @@ import time
 import requests
 import Adafruit_DHT
 
-sensor=Adafruit_DHT.DHT11
-gpio=17
+sensor = Adafruit_DHT.DHT11
+gpio = 17
 
 headers = {
-        'Fiware-Service' : 'ridsaert',
-        'Fiware-ServicePath' : '/udp_building',
-        'Content-Type' : 'application/json',
-    }
+    'Fiware-Service': 'ridsaert',
+    'Fiware-ServicePath': '/udp_building',
+    'Content-Type': 'application/json',
+}
+
 
 def send_values(temperature, humidity):
-    
     json_data = {
         'temperature': {
             'type': 'Integer',
@@ -23,14 +23,12 @@ def send_values(temperature, humidity):
             'value': humidity,
         },
     }
-    
+
     requests.post('http://20.16.84.167:1026/v2/entities/dht11/attrs', headers=headers, json=json_data)
 
 
-while(True):
-
+while (True):
     humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
     print('Temp {0:0.1f} C Humidity{1:0.1f}%'.format(temperature, humidity))
     send_values(temperature, humidity)
     time.sleep(300)
-
